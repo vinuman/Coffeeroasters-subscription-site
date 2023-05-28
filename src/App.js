5525c6ecc43ac60a721285e20e06bc65a2d6f8c7
Vinu Cyril <vinucyril@gmail.com>
2023-05-28 12:14:19 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { HashRouter as Router, Route, Routes, Link } from "react-router-dom";
 import logo from "./assets/shared/desktop/logo.svg";
 import Home from "./Home";
 import About from "./About";
